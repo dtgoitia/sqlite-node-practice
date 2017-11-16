8dbf715758ba45ae2e9116987d5634efc9a47b1c
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-11-16 09:36:17 +0000

    First successful query

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 // Source: http://www.sqlitetutorial.net/sqlite-nodejs/connect/
 
-
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
  
+const dbPath = path.resolve(__dirname, 'chinook.db');
 // open database in memory
-let db = new sqlite3.Database(':memory:', (err) => {
+let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
   if (err) {
-    return console.error(err.message);
+    console.error(err.message);
   }
-  console.log('Connected to the in-memory SQlite database.');
+  console.log('Connected to the chinook database.');
 });
+
+let sql = `SELECT DISTINCT Name name FROM playlists ORDER BY name`;
+
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  console.log('rows:', rows);
+})
  
 // close the database connection
 db.close((err) => {
